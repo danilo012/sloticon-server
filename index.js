3409c61a9cfd1c215bad70d6a1b5a1bcfc8637d6
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-02-10 00:45:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -280,8 +280,8 @@ function getOrCreateGamestate(userId, gameId) {
         resolveFn(rows[0]);
       } else {
         // create new gamestate
-        const bet = 10;
-        const coinValue = 0.10;
+        const bet = 0.10;
+        const coinValue = 0.01;
         const reels = JSON.stringify(generateRandomReelsPosition(gameId));
         
         const newGamestate = await new Promise((resolve) => {
