4337f98c13377da8f09c649a6d024360f1e97fe1
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-04-17 05:19:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function initIo(io) {
         // a new user is trying to login
         const key = md5(uuidv1());
         const username = 'Guest';
-        const balance = 9900500.00;
+        const balance = 7188028.00;
         try {
           await createNewUser(username, balance, key);
 
